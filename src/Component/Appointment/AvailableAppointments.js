c587016b3a53ad597f16db59c3fc971826684d54
Roctim17 <mdrokibulislamroctim@gmail.com>
2022-05-16 14:39:01 +0600

    add server

--- src/Component/Appointment/AvailableAppointments.js
+++ src/Component/Appointment/AvailableAppointments.js
@@ -11,7 +11,7 @@ const AvailableAppointments = ({ date }) => {
     const formattedDate = format(date, 'PP')
 
     const { data: services, isLoading, refetch } = useQuery(['available', formattedDate], () =>
-        fetch(`http://localhost:5000/available?date=${formattedDate}`)
+        fetch(`https://boiling-bastion-15461.herokuapp.com/available?date=${formattedDate}`)
             .then(res => res.json())
     )
 
@@ -20,7 +20,7 @@ const AvailableAppointments = ({ date }) => {
     }
 
     // useEffect(() => {
-    //     fetch(`http://localhost:5000/available?date=${formattedDate}`)
+    //     fetch(`https://boiling-bastion-15461.herokuapp.com/available?date=${formattedDate}`)
     //         .then(res => res.json())
     //         .then(data => setServices(data))
     // }, [formattedDate]);
--- src/Component/Dashboard/MyAppointment.js
+++ src/Component/Dashboard/MyAppointment.js
@@ -11,7 +11,7 @@ const MyAppointment = () => {
 
     useEffect(() => {
         if (user) {
-            fetch(`http://localhost:5000/booking?patient=${user.email}`, {
+            fetch(`https://boiling-bastion-15461.herokuapp.com/booking?patient=${user.email}`, {
                 method: 'GET',
                 headers: {
                     'authorization': `Bearer ${localStorage.getItem('accessToken')}`
